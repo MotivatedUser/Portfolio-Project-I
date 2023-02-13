fa1b6c75e9f0c2ed31d072b21accfbd3a6136fba
MotivatedUser <carbfixer@gmail.com>
2023-02-13 08:53:47 -0800

    Added gitignore

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(function() {
+
+
+    $("#scheduleButton").click(function() {
+        $("#scheduleModal").modal('show');
+    });
+    $("#scheduleButton2").click(function() {
+        $("#scheduleModal").modal('show');
+    });
+    $("#scheduleButton3").click(function() {
+        $("#scheduleModal").modal('show');
+    });
+    $("#scheduleButton4").click(function() {
+        $("#scheduleModal").modal('show');
+    });
+});
